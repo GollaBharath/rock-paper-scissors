bc60d72ece6d43110a444559af09aa04363a36e7
Golla Bharath <gollabharath2007@gmail.com>
2025-06-14 17:49:00 +0530

    Its done :)

--- script.js
+++ script.js
@@ -4,33 +4,53 @@ let ComputerScore = 0
 function GetComputerChoice(){
     let a = Math.random()*100
     if (a<33.33) {
-        return "Paper"
+        return "paper"
     }
     else if (a>66.66){
-        return "Rock"
+        return "rock"
     }
     else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
 function GetPlayerChoice(){
     let choice = prompt("Enter your choice : Rock/Paper/Scissors ")
-    return choice
+    if (choice === "Quit"){
+        process.exit()
+    }
+    return choice.toLowerCase()
 }
 
 function PlayRound(comp,player){
-    let order = ["Rock","Paper","Scissors"]
-    if (comp,player in order){
-        console.log("Works")
+    if (comp===player) {
+        return "Its a tie!!!"
+    }
+    if (comp === "rock" && player === "paper" || comp === "paper" && player === "scissors"){
+        HumanScore++
+        return "You Win!!!"
     }
     else {
-        console.log("Invalid Input")
+        ComputerScore++
+        return "You Lose!!!"
     }
 }
 
-let comp = GetComputerChoice()
-console.log("Computer Played :"+comp)
-let choice = GetPlayerChoice()
-console.log("You Played : "+choice)
-PlayRound(comp,choice)
+function Game() {
+    while (HumanScore < 5 && ComputerScore < 5) {
+        let comp = GetComputerChoice()
+        let choice = GetPlayerChoice()
+        console.log("Computer Played :"+comp)
+        console.log("You Played : "+choice)
+        let result = PlayRound(comp,choice)
+        console.log("Round Result : " + result)
+    }
+}
+
+Game()
+if (HumanScore > ComputerScore) {
+    console.log("Match Result : You Won!!!")
+}
+else {
+    console.log("Match Result : You Lose!!!")
+}
